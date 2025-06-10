a958464a67f946cf9518225e2211e7f963b79439
Markide1 <markndwiga@gmail.com>
2025-06-10 19:17:50 +0300

    Revert "[Added search by id, added cors]"
    This reverts commit 4c7dc79ddc0a026eb2fd517913761dcecc2861be.

--- books-api/src/main.ts
+++ books-api/src/main.ts
@@ -1,17 +1,8 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Enable CORS
-  app.enableCors({
-    origin: "http://127.0.0.1:8080",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    credentials: true,
-  });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
