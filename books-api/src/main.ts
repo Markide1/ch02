0d45367baef4f2773ca3e214ed2d6c745905214b
Markide1 <markndwiga@gmail.com>
2025-06-11 09:02:29 +0300

    [Fixed cors issue]

--- books-api/src/main.ts
+++ books-api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: "http://127.0.0.1:8080",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
